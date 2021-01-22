b410c7578df1f8e13031376be841b620bba65f4e
KaveeshaDilshan <kaveesha.18@cse.mrt.ac.lk>
2021-01-22 18:12:51 +0530

    order complete

--- services/expeditorServices.js
+++ services/expeditorServices.js
@@ -8,7 +8,7 @@ class expeditorService {
     static async showMaterials(){
         return expeditor.getMaterialValues();
     }
-    static async addNewOrder({final_materials,final_quantiies, optionProject}){
+    static async addNewOrder({final_materials,final_quantiies, optionProject, shop_name}){
         const project_id = await expeditor.getProjectId(optionProject);
         const today = new Date();
         // const day = ("0" + today.getDate()).slice(-2);
@@ -16,18 +16,43 @@ class expeditorService {
         // const year = today.getFullYear();  
         // const orderDate= day + '-' + month + '-' + year;
        
-        const o_id =(await expeditor.addToMaterialOrder(project_id.p_id, today)).o_id;
+        const o_id =(await expeditor.addToMaterialOrder(project_id.p_id, today, shop_name)).o_id;
         //  console.log(final_materials);
-        //  console.log(final_quantiies[0]);
-        try{
-        return expeditor.addToOrderItems(final_materials, final_quantiies,o_id);
-        }
-        catch{
-            console.log("errr")
-        }
+        //  console.log(final_quantiies);
+        //  console.log(o_id);
+        expeditor.addToOrderItems(final_materials, final_quantiies,o_id);
+        return (o_id);
+    }
+
+    static async sendOrder({o_id}){
+        return expeditor.changeOrderState(o_id);
+    }
+    static async deleteOrder({o_id}){
+        return expeditor.deleteOrder(o_id);
+    }
+    static async getOrdersAndEstimations({Project_name}){
+        const project_id = await expeditor.getProjectId(Project_name);
+       
+        const estimations = await expeditor.estimationsOfProject(project_id.p_id);
+        //  console.log(estimations[0]);
+        const orders = await expeditor.ordersOfProject(project_id.p_id);
+        // console.log(orders);
+        return [orders, estimations];
+    }
+    static async showCompleteOrder({o_id}){
+        const orderState = await expeditor.getOrderState(o_id);   
+        const orderItems = await expeditor.getOrderItems(o_id);  
+        // console.log(orderState);
+        // console.log(orderItems);
+        return [orderState,orderItems];   
+    }
+    static async showCompleteEstimate({e_id}){
+        const estimate_materials = await expeditor.getEstimate(e_id);  
+        // console.log(orderState);
+        // console.log(orderItems);
+        return estimate_materials;   
     }
     
-
     // static async showEstimate(){
     //     return QS.getEstimate();
     // }
